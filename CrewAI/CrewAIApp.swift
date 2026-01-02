ea9fef463ef1dbe38b345bd0cbea1fcf9869a209
Angshuman <angshumandas2611@gmail.com>
2026-01-02 23:01:20 +0530

    xcode project setup

---
+++ CrewAI/CrewAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewAIApp.swift
+//  CrewAI
+//
+//  Created by Angshuman on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct CrewAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
